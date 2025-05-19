1f5e353aae3ca52463568e40802819e08301ab7d
Martin Baran <baran@aps-brno.cz>
2025-05-19 21:27:57 +0200

    feat: add seed products data

--- ProductApi/Data/AppDbContext.cs
+++ ProductApi/Data/AppDbContext.cs
@@ -6,5 +6,20 @@ namespace ProductApi.Data
     public class AppDbContext: DbContext
     {
         public DbSet<Product> Products { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { ID = 1, Name = "Laptop", Price = 1200, Description = "Gaming laptop", Stock = 10 },
+                new Product { ID = 2, Name = "Keyboard", ImageUrl = "www.alza.cz", Price = 80 },
+                new Product { ID = 3, Name = "Mouse", ImageUrl = "www.alza.cz", Price = 50, Description = "Wireless mouse", Stock = 15 },
+                new Product { ID = 4, Name = "Smartphone", ImageUrl = "www.alza.cz", Price = 850, Description = "Wireless mouse", Stock = 4 },
+                new Product { ID = 5, Name = "Headphones", Price = 90, Description = "Nice headphones", Stock = 99 },
+                new Product { ID = 6, Name = "Lego", ImageUrl = "https://image.alza.cz/products/LO42207/LO42207.jpg?width=1400&height=1400", Price = 199.9M, Description = "Lego Technic 42207 Auto Ferrari SF-24 F1", Stock = 2 }
+            );
+        }
+
     }
 }
--- ProductApi/Models/Product.cs
+++ ProductApi/Models/Product.cs
@@ -4,7 +4,7 @@
     {
         public int ID { get; set; }
         public string Name { get; set; } = "";
-        public string Url { get; set; } = "";
+        public string ImageUrl { get; set; } = "";
         public decimal Price { get; set; }
         public string Description { get; set; } = "";
         public int Stock { get; set; }
