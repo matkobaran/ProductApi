ca931313fd61e3c19a19a3bbd1721e2c769cdd0c
Martin Baran <baran@aps-brno.cz>
2025-05-19 14:07:39 +0200

    feat: add product model

---
+++ ProductApi/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductApi.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = "";
+        public string Url { get; set; } = "";
+        public decimal Price { get; set; }
+        public string Description { get; set; } = "";
+        public int Quantity { get; set; }
+    }
+}
