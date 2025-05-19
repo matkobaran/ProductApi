e263b4c94c611608a69f4758205bc7c078f3507a
Martin Baran <baran@aps-brno.cz>
2025-05-19 19:44:50 +0200

    feat: add AppDbContext

---
+++ ProductApi/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Models;
+
+namespace ProductApi.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
