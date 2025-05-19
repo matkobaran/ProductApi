64a94e7de48653020f334a7a975d50145f84102b
Martin Baran <baran@aps-brno.cz>
2025-05-19 22:24:11 +0200

    feat: ef migrations add InitialCreate

---
+++ ProductApi/Migrations/20250519194517_InitialCreate.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ImageUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Stock = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ID", "Description", "ImageUrl", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 1, "Gaming laptop", "", "Laptop", 1200m, 10 },
+                    { 2, "", "www.alza.cz", "Keyboard", 80m, 0 },
+                    { 3, "Wireless mouse", "www.alza.cz", "Mouse", 50m, 15 },
+                    { 4, "Wireless mouse", "www.alza.cz", "Smartphone", 850m, 4 },
+                    { 5, "Nice headphones", "", "Headphones", 90m, 99 },
+                    { 6, "Lego Technic 42207 Auto Ferrari SF-24 F1", "https://image.alza.cz/products/LO42207/LO42207.jpg?width=1400&height=1400", "Lego", 199.9m, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
